059e3c97897416f7d2e1708368e33a2cba7cff76
kiana alikhan <kiana.prodesign@gmail.com>
2022-12-08 14:42:08 +0330

    add style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>TODO LIST</h1>
+      <div className="header-list">
+        <button className="add-task">Add Task</button>
+        <select className="selected">
+          <option>All</option>
+          <option>works</option>
+          <option>classes</option>
+          <option>celebraite</option>
+        </select>
+      </div>
+      <div className="container">
+        <div className="todo">
+          <input type="checkbox"></input>
+          <span>
+            <i className="fa fa-trash-o fa-border"></i>
+            <i class="fa fa-pencil fa-border" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div className="todo">
+          <input type="checkbox"></input>
+          <span>
+            <i className="fa fa-trash-o fa-border"></i>
+            <i class="fa fa-pencil fa-border" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div className="todo">
+          <input type="checkbox"></input>
+          <span>
+            <i className="fa fa-trash-o  fa-border"></i>
+            <i className="fa fa-pencil fa-border " aria-hidden="true"></i>
+          </span>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
